d451ab62da3c05c69d16d823375ff269d8073550
timotheG <gonin.timothe@gmail.com>
2024-10-07 10:14:50 +0200

    burger selection

--- flux.js
+++ flux.js
@@ -1,30 +1,97 @@
-let state = {}
 
+let state = {
+  value: null,
+  list: []
+};
 const subscribers = [];
-
-const subscribe = (funct) => {
-  return (subscribers.push(funct))
-}
 
 const dispatch = (newStateValue) => {
   state = newStateValue;
-  for(const funct of subscribers){
-    funct(state)
+  for (const fct of subscribers) {
+      fct(state)
   }
 }
 
-document.querySelector("#addForm").addEventListener('submit', e =>{
-  e.preventDefault();
-  const firstNameInput = e.currentTarget.firstName;
-  dispatch({
-    owner:{
-      firstName: firstNameInput.value
+const subscribe = (subscriberFct) => {
+  subscribers.push(subscriberFct);
+}
+
+subscribe((state) => {
+  console.log(state)
+  if (state.owner) {
+    console.log('Le propriétaire est ajouté', state.owner)
+    document.querySelector('#header').textContent = `Le propriétaire du restaurant est ${state.owner.firstName}`
+  }
+  if (state.list) {
+    document.querySelector('#command').innerHTML = ``;
+    for (let item of state.list) {
+      const itemElement = document.createElement('div')
+      itemElement.innerHTML = `
+      <div>
+      ${item.title} <span>${item.price}</span>
+      </div>
+      `
+      document.querySelector('#command').appendChild(itemElement)
     }
+  }
+})
+
+dispatch({
+  company: {
+      name: 'Burger du Pré'
+  },
+  list: []
+})
+
+
+document.querySelector('#addForm').addEventListener("submit", (evt) => {
+  evt.preventDefault()
+  const firstNameInput = evt.currentTarget.firstName
+  dispatch({
+      company: {
+          name: 'Burger du Pré'
+      },
+      owner: {
+          firstName: firstNameInput.value,
+      },
+      list: []
   })
 })
 
-subscribe(state => {
-  if(state){
-    document.querySelector('#header').textContent = `Le propriétaire du restaurant est ${state.owner.firstName}`
-  }
+document.querySelectorAll('.orderButton').forEach((element) => {
+  element.addEventListener('click', (event) => {
+      console.info(state)
+      const productId = event.target.dataset['id']
+      const productList = state.list
+      productList.push(PRODUCT_LIST[productId])
+      dispatch({
+          company: {
+              name: 'Burger du Pré'
+          },
+          list: productList,
+      })
+  })
 })
+
+const DoubleCantal = {
+  title: 'Double Cantal',
+  price: 15.99,
+}
+
+
+const SuperCremeux = {
+  title: 'Super Crémeux',
+  price: 14.99,
+}
+
+
+const PouletCroquant = {
+  title: 'Poulet Croquant',
+  price: 17.99,
+}
+
+const PRODUCT_LIST = {
+  PouletCroquant,
+  SuperCremeux,
+  DoubleCantal,
+}
